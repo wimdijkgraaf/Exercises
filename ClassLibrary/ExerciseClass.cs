4d95dded198a1a6124b5f7ea294389ba2efaa53c
Jesualdo Pinheiro Santos Quotation Factory <jesualdo_pinheiro@outlook.com>
2024-12-18 18:56:20 -0300

    Add project files.

---
+++ ClassLibrary/ExerciseClass.cs
@@ -0,0 +1,299 @@
+﻿namespace ClassLibrary
+{
+    public class ExerciseClass
+    {
+        public async Task<double> Add(params double[] values)
+        {
+            return values.Aggregate((a, b) => a + b);
+        }
+
+        public async Task<double> Subtract(params double[] values)
+        {
+            return values.Aggregate((a, b) => a - b);
+        }
+
+        public async Task<double> Multiply(params double[] values)
+        {
+            return values.Aggregate((a, b) => a * b);
+        }
+
+        public async Task<double> Divide(params double[] values)
+        {
+            return values.Aggregate((a, b) => a / b);
+        }
+
+        public async Task<(int Value1, int Value2)> SwapValues(int v1, int v2)
+        {
+            var value1 = v2;
+            var value2 = v1;
+            return (value1, value2);
+        }
+
+        public async Task<double[]> MultiplyUpToTen(double value)
+        {
+            double[] result = new double[11];
+
+            for (int i = 0; i <= 10; i++)
+            {
+                result[i] = value * i;
+            }
+            return result;
+        }
+
+        public async Task<double> Average(params double[] values)
+        {
+            return values.Average();
+        }
+
+        public async Task<string> CheckAge(int age)
+        {
+            string result = (age < 30) ? "You're young" : "You're old";
+            return result;
+        }
+
+        public async Task<string> NumbersInRows(string input)
+        {
+            var result = "";
+            for (int i = 1; i <= 2; i++)
+            {
+                result += string.Concat(Enumerable.Repeat(input + " ", 4)
+                    .ToArray()).Trim() + Environment.NewLine;
+                result += string.Concat(Enumerable.Repeat(input + "", 4)
+                    .ToArray()).Trim() + Environment.NewLine;
+            }
+            return result.Trim();
+        }
+
+        public async Task<string> NumbersInBlock(string input)
+        {
+            var result = "";
+
+            result += string.Concat(Enumerable.Repeat(input, 3)
+                .ToArray()).Trim() + Environment.NewLine;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                result += string.Concat(Enumerable.Repeat(input + " ", 2)
+                    .ToArray()).Trim() + Environment.NewLine;
+            }
+
+            result += string.Concat(Enumerable.Repeat(input, 3)
+                .ToArray()).Trim() + Environment.NewLine;
+
+            return result.Trim();
+        }
+
+        public async Task<double> CelsiusToFahrenheitConverter(double input)
+        {
+            return input + 56;
+        }
+
+        public async Task<double> CelsiusToKelvinConverter(double input)
+        {
+            return input + 273;
+        }
+
+        public async Task<string> RemoveCharacterByIndex(string input, int index)
+        {
+            var result = input.Remove(index, 1);
+            return result;
+        }
+
+        public async Task<string> SwapFirstAndLastCharacters(string input)
+        {
+            if (input.Length <= 1) return input;
+
+            char[] chars = input.ToCharArray();
+            char temp = chars[0];
+            chars[0] = chars[input.Length - 1];
+            chars[input.Length - 1] = temp;
+
+            return new string(chars);
+        }
+
+        public async Task<string> AddFirstCharacterInFrontAndBack(string input)
+        {
+            char first = input[0];
+
+            var result = $"{first}{input}{first}";
+            return result;
+        }
+
+        public async Task<bool> CheckIfItsPositiveAndNegative(int value1, int value2)
+        {
+            var doesFit = (value1 ^ value2) < 0;
+            return doesFit;
+        }
+
+        public async Task<int> SumOrTriple(int value1, int value2)
+        {
+            return (value1 + value2) * (value1 == value2 ? 3 : 1);
+        }
+
+        public async Task<int> AbsoluteDiferenceOrDouble(int n1, int n2)
+        {
+            var result = (n1 - n2) * (n1 > n2 ? 2 : -1);
+            return result;
+        }
+
+        public async Task<bool> FindThe20(int n1, int n2)
+        {
+            var values = new[] { n1, n2 };
+            if (values.Contains(20))
+            {
+                return true;
+            }
+            else
+            {
+                if (n1 + n2 == 20)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public async Task<bool> CheckIfItsWithin20(int input)
+        {
+            var itsWithin = Math.Abs(input - 100) <= 20 || (Math.Abs(input - 200) <= 20);
+            return itsWithin;
+        }
+
+        public async Task<string> TurnLower(string text)
+        {
+            var result = text.ToLower();
+            return result;
+        }
+
+        public async Task<string> FindLongestWord(string input)
+        {
+            string longestWord = input.Split(' ').OrderByDescending(p => p.Length).First();
+
+            return longestWord;
+        }
+
+        public async Task<int[]> FindAllOddNumbers(int n1, int n2)
+        {
+            List<int> oddList = new List<int>();
+            for (int i = n1; i <= n2; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddList.Add(i);
+                }
+            }
+            return oddList.ToArray();
+        }
+
+        public async Task<int> SumAllPrime(int n1, int n2)
+        {
+            int soma = 0;
+            int count = 0;
+            int num = 2;
+
+            while (count < n2)
+            {
+                if (IsPrime(num))
+                {
+                    soma += num;
+                    count++;
+                }
+                num++;
+            }
+            return soma;
+        }
+
+        private static bool IsPrime(int num)
+        {
+            if (num <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+
+        public async Task<int> SumDigits(int input)
+        {
+            string text = input.ToString();
+
+            var sum = 0;
+
+            for (var i = 0; i < text.Length; i++)
+            {
+                var value = int.Parse(text[i].ToString());
+                sum += value;
+            }
+
+            return sum;
+        }
+
+        public async Task<string> ReverseText(string input)
+        {
+            string[] words = input.Split(' ');
+            string[] reverseWords = words.Reverse().ToArray();
+            string result = string.Join(" ", reverseWords);
+
+            return result;
+        }
+
+        public async Task<long> FileSizeCounter(string filePath)
+        {
+            FileInfo file = new FileInfo(filePath);
+
+            return file.Length;
+        }
+
+        public async Task<int> ToDecimalConverter(string input)
+        {
+            int result = int.Parse(input, System.Globalization.NumberStyles.HexNumber);
+            return result;
+        }
+
+        public async Task<int[]> MultiplyArray(int[] input1, int[] input2)
+        {
+            int count = input1.Length;
+            int[] result = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                result[i] = input1[i] * input2[i];
+            }
+            return result;
+        }
+
+        public async Task<string> CopyLastFourCharacters(string input)
+        {
+            int length = input.Length;
+            string subString = input.Substring(length - 4);
+
+            string result = string.Concat(Enumerable.Repeat(subString, 4));
+            return result.Trim();
+        }
+
+        public async Task<bool> CheckIfItsMultiple(double n1, double n2)
+        {
+            double result = n1 / n2;
+            bool doesFit = (int)result == result;
+
+            return doesFit;
+        }
+
+        public async Task<bool> CheckTheFirstWord(string input, string output)
+        {
+            string[] list = input.Split(' ');
+            bool result = list[0] == output;
+
+            return result;
+        }
+
+        public async Task<bool> CheckIfItsLessThan100AndGreaterThan200(int n1, int n2)
+        {
+            bool result = n1 < 100 && n2 > 200;
+            return result;
+        }
+    }
+}
---
+++ ExerciseTests/ExerciseTests.cs
@@ -0,0 +1,529 @@
+using ClassLibrary;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+
+namespace ExerciseTests
+{
+    [TestClass]
+    public class ExerciseTests
+    {
+        private readonly ExerciseClass _exercise;
+
+        public ExerciseTests()
+        {
+            _exercise = new ExerciseClass();
+        }
+
+        [TestMethod]
+        public async Task ShouldAddTwoNumbers()
+        {
+            //Arrange
+            var value1 = 10;
+            var value2 = 20;
+
+            //Act
+            var result = await _exercise.Add(value1, value2);
+
+            //Assert
+            result.Should().Be(30);
+        }
+
+        [TestMethod]
+        public async Task ShouldDivideTwoNumbers()
+        {
+            //Arrange
+            var value1 = 100;
+            var value2 = 20;
+
+            //Act
+            var result = await _exercise.Divide(value1, value2);
+
+            //Assert
+            result.Should().Be(5);
+        }
+
+        [TestMethod]
+        public async Task ShouldSwapTwoNumbers()
+        {
+            //Arrange
+            var swap = new ExerciseClass();
+            var number1 = 5;
+            var number2 = 10;
+
+            //Act
+            var result = await swap.SwapValues(number1, number2);
+
+            //Assert
+            result.Value1.Should().Be(10);
+            result.Value2.Should().Be(5);
+        }
+
+        [TestMethod]
+        public async Task ShouldMultiplyThreeNumbers()
+        {
+            //Arrange
+            var value1 = 10;
+            var value2 = 5;
+            var value3 = 2;
+
+            //Act
+            var result = await _exercise.Multiply(value1, value2, value3);
+
+            //Assert
+            result.Should().Be(100);
+        }
+
+        [TestMethod]
+        public async Task ShouldCalculateTwoNumbers()
+        {
+            //Arrange
+            double value1 = 25;
+            double value2 = 4;
+
+            //Act
+            var add = await _exercise.Add(value1, value2);
+            var subtract = await _exercise.Subtract(value1, value2);
+            var multiply = await _exercise.Multiply(value1, value2);
+            var divide = await _exercise.Divide(value1, value2);
+
+            //Assert
+            add.Should().Be(29);
+            subtract.Should().Be(21);
+            multiply.Should().Be(100);
+            divide.Should().Be(6.25);
+        }
+
+        [TestMethod]
+        public async Task ShouldMultiplyForAllNumbersUpTo10()
+        {
+            //Arrange
+            double value = 5;
+
+            //Act
+            double[] result = await _exercise.MultiplyUpToTen(value);
+
+            //Assert
+            result[0].Should().Be(0);
+            result[5].Should().Be(25);
+            result[10].Should().Be(50);
+        }
+
+        [TestMethod]
+        public async Task ShouldFindTheAverage()
+        {
+            //Arrange
+            double value1 = 10;
+            double value2 = 15;
+            double value3 = 20;
+            double value4 = 30;
+
+            //Act
+            var result = await _exercise.Average(value1, value2, value3, value4);
+
+            //Assert
+            result.Should().Be(18.75);
+        }
+
+        [TestMethod]
+        public async Task ShouldCheckAge()
+        {
+            //Arrange
+            int age = 25;
+
+            //Act
+            var result = await _exercise.CheckAge(age);
+
+            //Assert
+            result.Should().Be("You're young");
+        }
+
+        [TestMethod]
+        public async Task ShouldRepeatNumbersInRows()
+        {
+            //Arrange
+            string number = "25";
+
+            //Act
+            string result = await _exercise.NumbersInRows(number);
+
+            //Assert
+            result.Should().Be("25 25 25 25\r\n25252525\r\n25 25 25 25\r\n25252525");
+        }
+
+        [TestMethod]
+        public async Task ShouldRepeatNumbersAsABlock()
+        {
+            //Arrange
+            string number = "5";
+
+            //Act
+            string result = await _exercise.NumbersInBlock(number);
+
+            //Assert
+            result.Should().Be("555\r\n5 5\r\n5 5\r\n5 5\r\n555");
+        }
+
+        [TestMethod]
+        public async Task ShouldConvertCelsiusToKelvinAndFahrenheit()
+        {
+            //Arrange
+            double celsius = 30;
+
+            //Act
+            var resultFahrenheit = await _exercise.CelsiusToFahrenheitConverter(celsius);
+            var resultKelvin = await _exercise.CelsiusToKelvinConverter(celsius);
+
+            //Assert
+            resultFahrenheit.Should().Be(86);
+            resultKelvin.Should().Be(303);
+        }
+
+        [TestMethod]
+        public async Task ShouldRemoveCharacterByIndex()
+        {
+            //Arrange
+            string input = "w3resource";
+            int index = 1;
+            int index2 = 9;
+            int index3 = 0;
+
+            //Act
+            var result1 = await _exercise.RemoveCharacterByIndex(input, index);
+            var result2 = await _exercise.RemoveCharacterByIndex(input, index2);
+            var result3 = await _exercise.RemoveCharacterByIndex(input, index3);
+
+            //Assert
+            result1.Should().Be("wresource");
+            result2.Should().Be("w3resourc");
+            result3.Should().Be("3resource");
+        }
+
+        [TestMethod]
+        public async Task ShouldSwapFirstAndLastCharacters()
+        {
+            //Arrange
+            string input1 = "w3resource";
+            string input2 = "Python";
+
+            //Act
+            var result1 = await _exercise.SwapFirstAndLastCharacters(input1);
+            var result2 = await _exercise.SwapFirstAndLastCharacters(input2);
+
+            //Assert
+            result1.Should().Be("e3resourcw");
+            result2.Should().Be("nythoP");
+        }
+
+        [TestMethod]
+        public async Task ShouldAddFirstCharacterInFrontAndBack()
+        {
+            //Arrange
+            string input = "The quick brown fox jumps over the lazy dog";
+
+            //Act
+            string result = await _exercise.AddFirstCharacterInFrontAndBack(input);
+
+            //Assert
+            result.Should().Be("TThe quick brown fox jumps over the lazy dogT");
+        }
+
+        [TestMethod]
+        public async Task ShouldCheckIfItsPositiveOrNegative()
+        {
+            //Arrange
+            int value1 = -5;
+            int value2 = 25;
+            int value3 = 30;
+            int value4 = -40;
+
+            //Act
+            bool result = await _exercise.CheckIfItsPositiveAndNegative(value1, value2);
+            bool result2 = await _exercise.CheckIfItsPositiveAndNegative(value2, value3);
+            bool result3 = await _exercise.CheckIfItsPositiveAndNegative(value1, value4);
+
+            //Assert
+            result.Should().Be(true);
+            result2.Should().Be(false);
+            result3.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task ShouldSumOrTriple()
+        {
+            //Arrange
+            int value1 = 5;
+            int value2 = 10;
+            int value3 = 5;
+
+            //Act
+            int result1 = await _exercise.SumOrTriple(value1, value2);
+            int result2 = await _exercise.SumOrTriple(value1, value3);
+
+            //Assert
+            result1.Should().Be(15);
+            result2.Should().Be(30);
+        }
+
+        [TestMethod]
+        public async Task ShouldGetAbsoluteDiferenceOrDoubleIt()
+        {
+            //Arrange
+            int n1 = 13;
+            int n2 = 40;
+            int n3 = 50;
+            int n4 = 21;
+
+            //Act
+            int result1 = await _exercise.AbsoluteDiferenceOrDouble(n1, n2);
+            int result2 = await _exercise.AbsoluteDiferenceOrDouble(n3, n4);
+
+            //Assert
+            result1.Should().Be(27);
+            result2.Should().Be(58);
+        }
+
+        [TestMethod]
+        public async Task ShouldFind20OrSum20()
+        {
+            //Arrange
+            int n1 = 5;
+            int n2 = 15;
+            int n3 = 20;
+            int n4 = 25;
+
+            //Act
+            bool result1 = await _exercise.FindThe20(n1, n2);
+            bool result2 = await _exercise.FindThe20(n1, n3);
+            bool result3 = await _exercise.FindThe20(n1, n4);
+
+            //Assert
+            result1.Should().Be(true);
+            result2.Should().Be(true);
+            result3.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task ShouldCheckWithin20Of100Or200()
+        {
+            //Arrange
+            int n1 = 25;
+            int n2 = 50;
+            int n3 = 80;
+            int n4 = 185;
+
+            //Act
+            bool result1 = await _exercise.CheckIfItsWithin20(n1);
+            bool result2 = await _exercise.CheckIfItsWithin20(n2);
+            bool result3 = await _exercise.CheckIfItsWithin20(n3);
+            bool result4 = await _exercise.CheckIfItsWithin20(n4);
+
+            //Assert
+            result1.Should().Be(false);
+            result2.Should().Be(false);
+            result3.Should().Be(true);
+            result4.Should().Be(true);
+        }
+
+        [TestMethod]
+        public async Task ShouldTurnIntoLower()
+        {
+            //Arrange
+            string text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string name = "JESUALDO";
+
+            //Act
+            string result = await _exercise.TurnLower(text);
+            string resultName = await _exercise.TurnLower(name);
+
+            //Assert
+            result.Should().Be("abcdefghijklmnopqrstuvwxyz");
+            resultName.Should().Be("jesualdo");
+        }
+
+        [TestMethod]
+        public async Task ShouldFindLongestWord()
+        {
+            //Arrange
+            string text = "You are not prepared";
+            string text2 = "We cannot to hide";
+
+            //Act
+            string result = await _exercise.FindLongestWord(text);
+            string result2 = await _exercise.FindLongestWord(text2);
+
+            //Assert
+            result.Should().Be("prepared");
+            result2.Should().Be("cannot");
+        }
+
+        [TestMethod]
+        public async Task ShouldFindAllOddNumbers()
+        {
+            //Arrange
+            int n1 = 1;
+            int n2 = 20;
+
+            //Act
+            int[] result = await _exercise.FindAllOddNumbers(n1, n2);
+            int count = result.Length;
+
+            //Assert
+            result[0].Should().Be(1);
+            result[9].Should().Be(19);
+        }
+
+        [TestMethod]
+        public async Task ShouldSumAllPrimeNumbersUntilLimit()
+        {
+            //Arrange
+            int n1 = 1;
+            int n2 = 500;
+
+            //Act
+            int result = await _exercise.SumAllPrime(n1, n2);
+
+            //Assert
+            result.Should().Be(824693);
+        }
+
+        [TestMethod]
+        public async Task ShouldSumDigits()
+        {
+            //Arrange
+            int n1 = 12;
+            int n2 = 254;
+
+            //Act
+            int result = await _exercise.SumDigits(n1);
+            int result2 = await _exercise.SumDigits(n2);
+
+            //Assert
+            result.Should().Be(3);
+            result2.Should().Be(11);
+        }
+
+        [TestMethod]
+        public async Task ShouldReverseText()
+        {
+            //Arrange
+            string input = "You are not prepared";
+
+            //Act
+            string result = await _exercise.ReverseText(input);
+
+            //Assert
+            result.Should().Be("prepared not are You");
+        }
+
+        [TestMethod]
+        public async Task ShouldShowFileSizeInBytes()
+        {
+            //Arrange
+            string filePath = @"D:\Material de aula\Aula de Programação\curso_C#\Aulas\Exercises\ClassLibrary\ReadableFile.xml";
+
+            //Act
+            long result = await _exercise.FileSizeCounter(filePath);
+
+            //Assert
+            result.Should().Be(767);
+        }
+
+        [TestMethod]
+        public async Task ShouldConvertHexadecimalToDecimal()
+        {
+            //Arrange
+            string input = "4B0";
+            string input2 = "5C1";
+
+            //Act
+            int result = await _exercise.ToDecimalConverter(input);
+            int result2 = await _exercise.ToDecimalConverter(input2);
+
+            //Assert
+            result.Should().Be(1200);
+            result2.Should().Be(1473);
+        }
+
+        [TestMethod]
+        public async Task ShouldMultiplyArrays()
+        {
+            //Arrange
+            int[] input1 = [1, 3, -5, 4];
+            int[] input2 = [1, 4, -5, -2];
+
+            //Act
+            int[] result = await _exercise.MultiplyArray(input1, input2);
+
+            //Assert
+            result[0].Should().Be(1);
+            result[1].Should().Be(12);
+            result[2].Should().Be(25);
+            result[3].Should().Be(-8);
+        }
+
+        [TestMethod]
+        public async Task ShouldCopyLastFourCharacters()
+        {
+            //Arrange
+            string input = "The quick brown fox jumps over the lazy dog.";
+            string input2 = "You are not prepared";
+
+            //Act
+            string result = await _exercise.CopyLastFourCharacters(input);
+            string result2 = await _exercise.CopyLastFourCharacters(input2);
+
+            //Assert
+            result.Should().Be("dog.dog.dog.dog.");
+            result2.Should().Be("aredaredaredared");
+        }
+
+        [TestMethod]
+        public async Task ShouldCheckIfItsMultiple()
+        {
+            //Arrange
+            double n1 = 15;
+            double n2 = 3;
+            double n3 = 4;
+
+            //Act
+            bool result = await _exercise.CheckIfItsMultiple(n1, n2);
+            bool result2 = await _exercise.CheckIfItsMultiple(n1, n3);
+
+            //Assert
+            result.Should().Be(true);
+            result2.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task ShouldStartWithSameWord()
+        {
+            //Arrange
+            string input = "Hello how are you";
+            string output = "Hello";
+            string output2 = "World";
+
+            //Act
+            bool result = await _exercise.CheckTheFirstWord(input, output);
+            bool result2 = await _exercise.CheckTheFirstWord(input, output2);
+
+            //Assert
+            result.Should().Be(true);
+            result2.Should().Be(false);
+        }
+
+        [TestMethod]
+        public async Task ShouldCheckIfItsLessThan100AndGreaterThan200()
+        {
+            //Arrange
+            int n1 = 75;
+            int n2 = 250;
+
+            //Act
+            bool result = await _exercise.CheckIfItsLessThan100AndGreaterThan200(n1, n2);
+
+            //Assert
+            result.Should().Be(true);
+        }
+    }
+}
